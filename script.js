7d468de96fa035b97ead6b4c91c4e03652983967
riccardo <castoldi.riccardo@iisgalvanimi.edu.it>
2025-11-14 09:37:10 +0000

    Aggiungi la funzione buttonFunction1 per cambiare i colori sul canvas

--- script.js
+++ script.js
@@ -1,21 +1,37 @@
 function buttonFunction() {
   var canvas = document.getElementById("myCanvas");
   var ctx = canvas.getContext("2d");
-  ctx.fillStyle = "#FF0000";
-  ctx.fillRect(0, 0, 200, 200);
-  ctx.fillStyle = "#00FF00";
-
-  ctx.fillRect(0, 0, 10, 10);
+  ctx.fillStyle = "#24afdaff";
+  ctx.fillRect(20, 20, 150, 75);
+  ctx.fillStyle = "#5e2f03ff";
+ctx.fillRect(40, 40, 10, 10);
 
   ctx.fillRect(20, 20, 10, 10);
 
-  ctx.fillRect(40, 40, 10, 10);
+   
+} 
 
-    ctx.fillRect(60, 60, 10, 10);
-    ctx.fillRect(80, 80, 10, 10);
-    ctx.fillRect(100, 100, 10, 10);
-    ctx.fillRect(120, 120, 10, 10);
-    ctx.fillRect(140, 140, 10, 10);
-    ctx.fillRect(160, 160, 10, 10);
-    ctx.fillRect(180, 180, 10, 10);
+function buttonFunction1() {
+  var canvas = document.getElementById("myCanvas");
+  var ctx = canvas.getContext("2d");
+  ctx.fillStyle = "#f0f0f0ff";
+  ctx.fillRect(20, 20, 150, 75);
+  ctx.fillStyle = "#7e2c73ff";
+
+    ctx.fillRect(20, 20, 10, 10,);
+    ctx.fillRect(120, 20, 10, 10,);
+    ctx.fillRect(40,40,10,10);
+    ctx.fillRect(60,60,10,10);
+    ctx.fillRect(80,80,10,10);
+    ctx.fillRect(120,120,10,10);
+    ctx.fillRect(100,100,10,10);
+    ctx.fillRect(20,120,10,10);
+    ctx.fillRect(100,40,10,10);
+    ctx.fillRect(80,60,10,10);
+    ctx.fillRect(60,80,10,10);
+    ctx.fillRect(40,100,10,10);
+
+
+
+
 }
