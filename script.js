703a5efce8069885e9b6e8e4106ba1ca27e0d46f
riccardo <castoldi.riccardo@iisgalvanimi.edu.it>
2025-11-14 09:51:54 +0000

    Aggiungi la funzione buttonFunction2 per cambiare forma sul canvas

--- script.js
+++ script.js
@@ -17,20 +17,25 @@ function buttonFunction1() {
   ctx.fillStyle = "#f0f0f0ff";
   ctx.fillRect(20, 20, 150, 75);
   ctx.fillStyle = "#7e2c73ff";
-
-    ctx.fillRect(20, 20, 10, 10,);
-    ctx.fillRect(120, 20, 10, 10,);
-    ctx.fillRect(40,40,10,10);
-    ctx.fillRect(60,60,10,10);
-    ctx.fillRect(80,80,10,10);
-    ctx.fillRect(120,120,10,10);
-    ctx.fillRect(100,100,10,10);
-    ctx.fillRect(20,120,10,10);
-    ctx.fillRect(100,40,10,10);
-    ctx.fillRect(80,60,10,10);
-    ctx.fillRect(60,80,10,10);
-    ctx.fillRect(40,100,10,10);
-
+for (let i = 20; i < 130; i += 20){
+    ctx.fillRect(i, i, 10, 10,);
+}
+   
+for (let i = 20; i < 130; i += 20){
+    ctx.fillRect(140-i, i, 10, 10,);
+}
+function buttonFunction2() {
+  var canvas = document.getElementById("myCanvas");
+  var ctx = canvas.getContext("2d");
+  ctx.fillStyle = "#f0f0f0ff";
+  ctx.fillRect(20, 20, 150, 75);
+  ctx.fillStyle = "#7e2c73ff";
+for (let i = 20; i < 130; i += 20){
+   for (let j = 20; j < 130; j += 20){
+    ctx.fillRect(j, i, 10, 10,);
+}
+}
+   
 
 
 
