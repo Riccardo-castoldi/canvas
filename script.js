e789c39aefcef5301d9a0e8e5a4a5e1e194b7e30
riccardo <castoldi.riccardo@iisgalvanimi.edu.it>
2025-11-14 09:01:48 +0000

    Aggiungi la funzione buttonFunction al pulsante e aggiorna il disegno sul canvas

--- script.js
+++ script.js
@@ -2,10 +2,12 @@ function buttonFunction() {
   var canvas = document.getElementById("myCanvas");
   var ctx = canvas.getContext("2d");
   ctx.fillStyle = "#FF0000";
-  ctx.fillRect(0, 0, 150, 75);
+  ctx.fillRect(0, 0, 200, 200);
   ctx.fillStyle = "#00FF00";
 
   ctx.fillRect(0, 0, 10, 10);
 
   ctx.fillRect(20, 20, 10, 10);
+
+  ctx.fillRect(40, 40, 10, 10);
 }
